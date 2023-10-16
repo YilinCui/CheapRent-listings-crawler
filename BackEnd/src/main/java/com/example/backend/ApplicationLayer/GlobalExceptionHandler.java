6074b4cbdbd7b963acb989d0b1b25086f028b046
Yilin Cui <yc4762@nyu.edu>
2023-10-16 15:28:41 -0700

    Add GlobalExceptionHandler

--- BackEnd/src/main/java/com/example/backend/ApplicationLayer/GlobalExceptionHandler.java
+++ BackEnd/src/main/java/com/example/backend/ApplicationLayer/GlobalExceptionHandler.java
@@ -1,23 +1,18 @@
-//package com.example.backend.ApplicationLayer;
-//
-//import com.example.backend.Entity.Rental;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.context.request.WebRequest;
-//import org.springframework.web.servlet.NoHandlerFoundException;
-//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-//
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-//
-//    protected ResponseEntity<Object> ResponseEntityExceptionHandler(
-//            NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-//
-//        Rental rental = new Rental();
-//
-//        return new ResponseEntity<>(rental, HttpStatus.OK);
-//    }
-//}
+package com.example.backend.ApplicationLayer;
+
+import com.example.backend.Entity.Rental;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Rental> handleAllExceptions(Exception ex, WebRequest request) {
+        Rental rental = new Rental();
+        return new ResponseEntity<>(rental, HttpStatus.BAD_REQUEST);
+    }
+}
